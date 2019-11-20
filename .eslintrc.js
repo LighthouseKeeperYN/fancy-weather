5943741c4d0e691d00ac7021de3de14bdaf342f9
LighthouseKeeperYN <freesaber@yandex.ru>
2019-11-20 17:31:49 +0300

    feat: setup environment

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+        "extends": [
+            "airbnb-base",
+            "prettier"
+        ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly",
+        "firebase": false
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+      "no-param-reassign": 0,
+      "import/extensions": 0,
+      "no-multi-assign": 0,
+      "no-plusplus": 0,
+      "no-use-before-define": 0,
+      "no-bitwise": 0,
+      "no-constant-condition": [
+        "error",
+        {
+          "checkLoops": false
+        }
+      ],
+      "prefer-destructuring": 0,
+      "no-alert": 0
+    }
+};
---
+++ fancy-weather/js/main.js
@@ -0,0 +1,35 @@
+import '../sass/main.scss';
+import {
+  moduleOne
+} from './moduleOne';
+
+const helloArr = require('./moduleOne.js');
+
+
+console.log('API Key from Define Plugin:', API_KEY);
+
+class TestClass {
+  constructor() {
+    const msg = "Using ES2015+ syntax";
+    console.log(msg);
+  }
+}
+
+let test = new TestClass();
+
+
+// Пример массива
+console.log(helloArr);
+
+
+// Пример работы с глобальными переменными
+if (APP_ENV === 'dev') {
+  //код только при отладке
+  console.log("development hello!");
+}
+
+
+/* пример подключения модуля*/
+let mod = moduleOne(2, 3);
+
+console.log(mod);
