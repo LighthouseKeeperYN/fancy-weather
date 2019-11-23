152fc41eba944fad853aa3322ebd824c65c82a13
LighthouseKeeperYN <freesaber@yandex.ru>
2019-11-23 17:37:53 +0300

    feat: country and city rendering

--- fancy-weather/js/location.js
+++ fancy-weather/js/location.js
@@ -1,5 +1,3 @@
-// import { apiKeys, settings } from './globals';
-
 export const countryAndCity = document.createElement('p');
 countryAndCity.classList.add('weather-data-cluster__location');
 countryAndCity.innerText = 'Minsk, Belarus';
@@ -15,28 +13,3 @@ latitude.innerText = 'Latitude: 53°54\'';
 longitude.innerText = 'Longitude: 27°34\'';
 coordinates.appendChild(latitude);
 coordinates.appendChild(longitude);
-
-
-
-
-
-// export class GeoData {
-//   constructor(place) {
-//     this.apiKey = 'dfcea8096a95496ba653f501109c66bf';
-//     this.place = place;
-//   }
-
-//   async getData() {
-//     const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${this.place}&key=${this.apiKey}&pretty=1&no_annotations=1`);
-
-//     const responseData = await response.json();
-//     console.log(responseData.results[0]);
-//     return {
-//       latitude: responseData.results[0].geometry.lat,
-//       longitude: responseData.results[0].geometry.lng
-//     }
-//   }
-// }
-
-// const getGeoData = new GeoData(224020);
-// getGeoData.getData().then( data => console.log(data));
--- fancy-weather/js/main.js
+++ fancy-weather/js/main.js
@@ -1,10 +1,4 @@
-// console.log('API Key from Define Plugin:', API_KEY);
-
-// // Пример работы с глобальными переменными
-// if (APP_ENV === 'dev') {
-//   //код только при отладке
-//   console.log("development hello!");
-// }
-
 import '../sass/main.scss';
+
 import './domBuilder';
+import './ui';
