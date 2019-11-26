313df5b0f4febceaae19efb2177b30d0787a6165
LighthouseKeeperYN <freesaber@yandex.ru>
2019-11-27 01:04:01 +0300

    refactor: attempt to make try catch mess in fetchers more readable

--- fancy-weather/js/fetchers.js
+++ fancy-weather/js/fetchers.js
@@ -14,9 +14,7 @@ export async function getGeoData(place, apiKey, language) {
   try {
     response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${place}&key=${apiKey}&pretty=1&no_annotations=1&language=${language}`);
     responseData = await response.json();
-  } catch (error) {
-    console.log('GEODATA:', error);
-  }
+  } catch (error) { console.log('GEODATA:', error); }
 
   if (responseData.results.length === 0) return false;
 
@@ -39,9 +37,8 @@ export async function getIPInfo(key) {
   try {
     response = await fetch(`https://cors-anywhere.herokuapp.com/https://ipinfo.io/json?token=${key}`);
     responseData = await response.json();
-  } catch (error) {
-    console.log('IPINFO', error);
-  }
+  } catch (error) { console.log('IPINFO', error); }
+
   return responseData;
 }
 
@@ -54,19 +51,21 @@ export async function getImageURL(keywords, apiKeys) {
     response = await fetch(`https://api.unsplash.com/photos/random?query=${keywords[0]},${keywords[1]},${keywords[2]}&client_id=${key}&orientation=landscape`);
 
     for (let i = 0; response.status === 403 && i < apiKeys.keys.length; i++) {
-      key = apiKeys.updateKey();
-      // eslint-disable-next-line no-await-in-loop
-      response = await fetch(`https://api.unsplash.com/photos/random?query=${keywords[0]},${keywords[1]},${keywords[2]}&client_id=${key}&orientation=landscape`);
-    }
-  } catch (error) {
-    console.log('UNSPLASH-1: ', error);
-  }
 
-  try {
-    responseData = await response.json();
-  } catch (error) {
-    console.log('UNSPLASH-2: ', error);
-  }
+      try {
+        key = apiKeys.updateKey();
+        // eslint-disable-next-line no-await-in-loop
+        response = await fetch(`https://api.unsplash.com/photos/random?query=${keywords[0]},${keywords[1]},${keywords[2]}&client_id=${key}&orientation=landscape`);
+
+      }
+      catch (error) { console.log('UNSPLASH-loop: ', error); }
+
+    }
+
+  } catch (error) { console.log('UNSPLASH-1: ', error); }
+
+  try { responseData = await response.json(); }
+  catch (error) { console.log('UNSPLASH-2: ', error); }
 
   return responseData.urls.regular;
 }
